7613fe4bff7a3aac7c94b4ee0ddcf5fa69146e4f
Judy Tsai <sonic8776@gmail.com>
2024-06-20 21:11:49 +0800

    Implement code structure of URLSessionHTTPClientTest

---
+++ JTNetworkTests/URLSessionHTTPClientTest.swift
@@ -0,0 +1,22 @@
+//
+//  URLSessionHTTPClientTest.swift
+//  JTNetworkTests
+//
+//  Created by Judy Tsai on 2024/6/20.
+//
+
+import XCTest
+@testable import JTNetwork
+
+class URLSessionHTTPClientTest: XCTestCase {
+    
+}
+
+// MARK: - Helpers
+private extension URLSessionHTTPClientTest {
+}
+
+// MARK: - Factory Methods
+private extension URLSessionHTTPClientTest {
+    
+}
