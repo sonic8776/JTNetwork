ba50cb8e0a5923f0e5e9f70e27916594dab20c68
Judy Tsai <sonic8776@gmail.com>
2024-06-20 21:12:09 +0800

    Implement URLProtocol stub

--- JTNetworkTests/URLSessionHTTPClientTest.swift
+++ JTNetworkTests/URLSessionHTTPClientTest.swift
@@ -14,6 +14,59 @@ class URLSessionHTTPClientTest: XCTestCase {
 
 // MARK: - Helpers
 private extension URLSessionHTTPClientTest {
+    struct RequestAndResponeStub {
+        let data: Data?
+        let response: URLResponse?
+        let error: Error?
+    }
+    
+    class URLProtocolStub: URLProtocol {
+        private static var stub: RequestAndResponeStub?
+        
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = RequestAndResponeStub.init(data: data, response: response, error: error)
+        }
+        
+        static func startInterceptingRequest() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptingRequest() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            stub = nil
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        override func startLoading() {
+            guard let stub = URLProtocolStub.stub else {
+                client?.urlProtocolDidFinishLoading(self)
+                return
+            }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = stub.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() {}
+    }
 }
 
 // MARK: - Factory Methods
